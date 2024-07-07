632baeb2b375ebfd6a5f0e271c0d4eb998628d2d
Uriel Sarmiento <uriel.sarmiento.solano@gmail.com>
2024-07-07 08:29:45 -0600

    Add Employee feature added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import './App.css'
-import ListEmployeeComponent from './assets/components/ListEmployeeComponent'
+import './App.css';
+import EmployeeComponent from './assets/components/EmployeeComponent';
+import FooterComponent from './assets/components/FooterComponent';
+import HeaderComponent from './assets/components/HeaderComponent';
+import ListEmployeeComponent from './assets/components/ListEmployeeComponent';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
-      <ListEmployeeComponent/>
+      <BrowserRouter>
+        <HeaderComponent />
+          <Routes>
+            {/*http://localhost:3000*/}
+            <Route path='/' element = { <ListEmployeeComponent/> }></Route>
+            {/*http://localhost:3000/employees*/}
+            <Route path='/employees' element = { <ListEmployeeComponent/> }></Route>
+            {/*http://localhost:3000/add-employee*/}
+            <Route path='/add-employee' element = { <EmployeeComponent/> }></Route>
+          </Routes>
+        <FooterComponent />
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/assets/components/FooterComponent.jsx
@@ -0,0 +1,12 @@
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className="footer">
+            <span>Made with React JS and Java Springboot</span>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
